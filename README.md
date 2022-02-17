140de84577cc6e1f04312911c9726f990db76652
Kirubakaran Shanmugam <kribakarans@gmail.com>
2022-02-17 12:17:20 +0530

    Implemented install.sh

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # dotfile-installer
 Dotfile installer
+
+**Feb 17 2022 11:55**
+- Implemented ```install.sh``` script to clone [dotman](https://github.com/atomixcloud/dotman) and install dotfiles in the system
+
---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+if [ "$GITHUB_ACCESSTOKEN" == "" ]; then
+	read -p "Enter GitHub access token: " GITHUB_ACCESSTOKEN
+fi
+
+BUILDPATH=$PREFIX/tmp
+HOST=https://$GITHUB_ACCESSTOKEN@github.com
+
+#URL=$HOST/atomixcloud/rcupdate.git
+URL=git@github.com:atomixcloud/dotman.git
+
+NAME=$(basename $URL)
+TIME=$(date +"%Y%m%d%H%M%S")
+BRANCH=$BUILDPATH/$NAME.$TIME
+
+echo "Installing dotfiles ..."
+git clone $URL $BRANCH
+
+cd $BRANCH
+eval ./rcupdate.sh
+cd $OLDPWD
+rm -rf $BRANCH
+
+exit $?
